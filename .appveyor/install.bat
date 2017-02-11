a26b23ca181489a69c6ed255ab7e16b72ae7fa82
Christoph Auer <auer.christoph@outlook.at>
2017-02-11 13:22:01 +0100

    Organize in methods

--- .appveyor/install.bat
+++ .appveyor/install.bat
@@ -2,39 +2,63 @@ SET BUILD_DIR=C:\TBuild
 SET OPEN_SSL_BRANCH=OpenSSL_1_0_1-stable
 SET LZMA_FILE=lzma920.tar.bz2
 
-:: Create libraries folder
-cd ..
-mkdir Libraries
-cd Libraries
+call:installUtils
+call:createLibsFolder
+call:getOpenSSL
+call:getLZMA
 
-:: OpenSSL
-git clone https://github.com/openssl/openssl.git
-cd openssl
-git checkout %OPEN_SSL_BRANCH%
-perl Configure VC-WIN32 --prefix=%BUILD_DIR%\Libraries\openssl\Release
-:: TODO this is failing
-::ms\do_ms
-::nmake -f ms\nt.mak
-::nmake -f ms\nt.mak install
-cd ..
-dir
+echo Install completed
 
-git clone https://github.com/openssl/openssl.git openssl_debug
-cd openssl_debug
-git checkout %OPEN_SSL_BRANCH%
-perl Configure debug-VC-WIN32 --prefix=%BUILD_DIR%\Libraries\openssl_debug\Debug
-:: TODO this is failing
-::ms\do_ms
-::nmake -f ms\nt.mak
-::nmake -f ms\nt.mak install
-cd ..
-dir
-
-:: LZMA SDK 9.20
-powershell -Command "Invoke-WebRequest http://downloads.sourceforge.net/sevenzip/%LZMA_FILE% -OutFile %LZMA_FILE%"
-dir
-tar xaf %LZMA_FILE%
-:: TODO Unzip lzma sdk
+GOTO:EOF
 
 :: List libs
 dir
+
+:: FUNCTIONS
+
+:installUtils
+    echo Install utils
+    choco install -y curl 7zip
+
+    if not "%VSVER%" == "" call "C:\Program Files (x86)\Microsoft Visual Studio %VSVER%.0\VC\vcvarsall" x86
+    set PATH=%PATH%;"C:\Program Files\7-Zip";%TOOLSDIR%\bin
+GOTO:EOF
+
+:createLibsFolder
+    echo Create libs folder
+    cd ..
+    mkdir Libraries
+    cd Libraries
+GOTO:EOF
+
+:getOpenSSL
+    echo Get open ssl
+    git clone https://github.com/openssl/openssl.git
+    cd openssl
+    git checkout %OPEN_SSL_BRANCH%
+    perl Configure VC-WIN32 --prefix=%BUILD_DIR%\Libraries\openssl\Release
+    :: TODO this is failing
+    ::ms\do_ms
+    ::nmake -f ms\nt.mak
+    ::nmake -f ms\nt.mak install
+    cd ..
+    dir
+
+    git clone https://github.com/openssl/openssl.git openssl_debug
+    cd openssl_debug
+    git checkout %OPEN_SSL_BRANCH%
+    perl Configure debug-VC-WIN32 --prefix=%BUILD_DIR%\Libraries\openssl_debug\Debug
+    :: TODO this is failing
+    ::ms\do_ms
+    ::nmake -f ms\nt.mak
+    ::nmake -f ms\nt.mak install
+    cd ..
+    dir
+GOTO:EOF
+
+:getLZMA
+    echo Get lzma sdk
+    powershell -Command "Invoke-WebRequest http://downloads.sourceforge.net/sevenzip/%LZMA_FILE% -OutFile %LZMA_FILE%"
+    dir
+    :: TODO Unzip lzma sdk
+GOTO:EOF
